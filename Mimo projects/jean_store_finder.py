9583def433e74d18d2c34ecf09f82d1b1cac25ef
[christiangtorres1] <christiangtorres23@gmail.com>
2022-08-31 22:11:54 -0500

    Modification to Jean inventory app

--- Mimo projects/jean_store_finder.py
+++ Mimo projects/jean_store_finder.py
@@ -1,4 +1,5 @@
-# This app is made to check the inventory of the jeans store.
+# This app is made to check the inventory and goals of the jeans store.
+# To run change the integers and run application.
 
 sales = 500
 initial_inventory = 750
@@ -6,19 +7,23 @@ target = 450
 current_inventory = initial_inventory - sales
 hit_target = True
 
-print('August 2022 Sales report')
-print(f'Sales this month were {sales}')
-
-if hit_target == True:
-    print('We did it')
-else:
-    print("We need to sell more jeans!")
+print('August 2022 Sales report:')
+print(f'Sales this month were {sales} pairs of jeans.')
 
 print('Target report:')
-print('Our target was 450 jeans sold')
+
+print(f'Our target was {target}')
+
+if hit_target == True:
+    print(f'We hit our goal of {target} jeans by {sales - target}.')
+else:
+    print(f'We need to sell more jeans! We are short {target - sales}')
+
+
 if hit_target == True:
     print(f'We beat our target by {sales - target}')
 else:
     print(f'We are short {target - sales}')
 
+print('Inventory Report:')
 print(f'We now have an inventory of {current_inventory}')
