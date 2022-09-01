345151196dfd51a2ade3c10521809aadc6dd3b27
[christiangtorres1] <christiangtorres23@gmail.com>
2022-08-31 21:32:25 -0500

    adding mile to kilometers calculator

--- Mimo projects/miles_calculator.py
+++ Mimo projects/miles_calculator.py
@@ -1,6 +1,7 @@
 ## This application converts miles to kilometers
 
-miles = 500
+#Change the miles to the number you're looking for and run
+miles = 80004
 kilometers = miles * 1.60934
 
-print(kilometers)
+print(f'The answer is {kilometers} kilometers')
