0d241a9152817b2423da4afbd2ca2590c1960142
[christiangtorres1] <christiangtorres23@gmail.com>
2022-09-01 19:52:33 -0500

    A few movement changes

---
+++ Mimo project/miles_calculator.py
@@ -0,0 +1,7 @@
+## This application converts miles to kilometers
+
+#Change the miles to the number you're looking for and run
+miles = 80004
+kilometers = miles * 1.60934
+
+print(f'The answer is {kilometers} kilometers')
