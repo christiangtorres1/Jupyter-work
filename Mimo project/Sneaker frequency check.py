99e8ca89d0a459c24e793b7b40b65a857d77e32d
[christiangtorres1] <christiangtorres23@gmail.com>
2022-09-01 20:04:36 -0500

    finished sneaker frequency app

---
+++ Mimo project/Sneaker frequency check.py
@@ -0,0 +1,14 @@
+#This app checks to see how frequent shoes are used
+
+frequency = "once a week"
+intensity = 'high'
+
+highly_active = frequency == "daily"
+print('If True, frequency is daily, if False frequency is weekly:')
+print(highly_active)
+
+high_intensity = intensity == "high"
+print('If True, intensity is high, if False intensity is low:')
+print(high_intensity
+
+      )
